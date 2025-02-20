a9180372c59d8622bce8c13989420223463a8260
Stephen Yeung <yeohstephen@live.hk>
2025-02-20 18:17:07 +0800

    Add function call api

--- scripts/chatclient.js
+++ scripts/chatclient.js
@@ -3,14 +3,18 @@ import { Ollama } from './ollama/browser.mjs';
 export function ChatClient(url) { 
 	this.Host = url;
 	this.Dialogue = []; 
+	this.recevStr = "";
 		
+	this.ollama = null; 
+		
+	//event	
 	this.onBegin = null;	
 	this.onEnd = null;
+	this.onResult = null;
 	this.onReceive = null;	
-	this.onDispose = null;
+	this.onDispose = null; 
 	
-	this.ollama = null; 
-	
+	//Get model list
 	this.GetModels = async function() {
 		this.checkOllama();
 			
@@ -24,52 +28,72 @@ export function ChatClient(url) {
 		return list; 
 	}
 	
-	this.Send = async function(llm, message) { 		
+	//chat
+	this.Send = async function(llm, message, continuous, functions) { 		
 		this.checkOllama();
 		
 		this.Dialogue.push({ role: 'user', content: message });	
-		let recvStr = "";		
+		this.recevStr = "";		
+		
+		if (this.onBegin != null) {
+			this.onBegin();
+		}
 		
 		//create chat
 		const response = await this.ollama.chat({
 			model: llm,
 			messages: this.Dialogue,
-			stream: true
+			stream: continuous, 
+			keep_alive: '10m', 
+			options: { num_ctx: 4096, temperature: 0 },
+			tools: functions
 		});	 		
 		
-		if (this.onBegin != null) {
-			this.onBegin();
+		if (this.onResult != null) {
+			this.onResult();
 		}
 					
-		// get message
-		for await (const part of response) {
-			recvStr += part.message.content;
-			
+		if (continuous) {
+			// get message
+			for await (const part of response) { 			
+				this.recevStr += part.message.content;			
+				 
+				if (this.onReceive != null) {
+					this.onReceive(part.message.content);
+				}		 
+				
+				if (part.done) {
+					if (this.onEnd != null) {
+						this.onEnd(part);
+					}
+				}
+			}	 
+		}	
+		else 
+		{
 			if (this.onReceive != null) {
-				this.onReceive(part.message.content);
+				this.onReceive(response.message.content);
+			}	
+
+			if (response.done) {
+				if (this.onEnd != null) {
+					this.onEnd(response);
+				}
 			}
-		}
+		}	
 		
 		//add message to loop
-		this.Dialogue.push({ role: 'assistant', content: recvStr }); 
-		
-		if (this.onEnd != null) {
-			this.onEnd();
-		}
+		this.Dialogue.push({ role: 'assistant', content: this.recevStr });  
 	}
 	
+	//abort
 	this.Stop = function() {
 		if (this.ollama != null) {
 			this.ollama.abort();
 		}
 	}
 	
-	this.checkOllama = function() {
-		if (this.ollama == null) {
-			this.ollama = new Ollama({ host: this.Host });
-		}
-	}	
-	
+	//delete chat
 	this.Reset = function() {
 		this.Dialogue.length = 0;
 		
@@ -77,4 +101,10 @@ export function ChatClient(url) {
 			this.onDispose();
 		}			
 	}
+	
+	this.checkOllama = function() {
+		if (this.ollama == null) {
+			this.ollama = new Ollama({ host: this.Host });
+		}
+	} 
 };
